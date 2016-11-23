70d33eb634c4de7fc73d807734724d3f084f5885
Gustavo Puglia <gpugliac@gmail.com>
2016-11-23 00:24:26 +0000

    Add new friend action

--- app/models/friend.js
+++ app/models/friend.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   firstName: DS.attr('string'),
-  lastName: DS.attr('string')
+  lastName: DS.attr('string'),
+  email: DS.attr('string'),
+  twitter: DS.attr('string'),
 });
